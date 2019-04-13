b70fdd6bd4a5e7d8377f0cc1dda05de4f8a6a430
JD <jdfili14@gmail.com>
2019-04-13 10:01:26 -0700

    update npm

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -3,28 +3,28 @@ var router = express.Router();
 var db = require("../models");
 
 router.get("/", function (req, res) {
-  db.Burger.findAll({}).then(function(dbBurger){
-    var hbsObject = {
-        burgers: dbBurger
-    };
-    res.render("index", hbsObject)
-  })
- 
+    db.Burger.findAll({}).then(function (dbBurger) {
+        var hbsObject = {
+            burgers: dbBurger
+        };
+        res.render("index", hbsObject)
+    })
+
 });
 router.post("/api/burgers", function (req, res) {
-    db.Burger.create(req.body).then(function(dbBurger) {
+    db.Burger.create(req.body).then(function (dbBurger) {
         res.json(dbBurger);
-      });
+    });
 })
 
 router.put("/api/burgers/:id", function (req, res) {
     db.Burger.update({
         devoured: req.body.devour
-    },{
+    }, {
         where: {
             id: req.params.id
         }
-    }).then(function(dbBurger){
+    }).then(function (dbBurger) {
         res.json(dbBurger);
     });
 });
